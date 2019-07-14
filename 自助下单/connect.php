eac3694616e17787de835a49bfcab3fa47ea1694
KIKI <46512207+raingnight@users.noreply.github.com>
2019-07-14 11:33:23 +0800

    数据库连接信息

--- 自助下单/connect.php
+++ 自助下单/connect.php
@@ -1,9 +1,10 @@
 <?php
+//定义数据库连接信息
 	define('DB_HOST', 'localhost');  
     define('DB_USER', 'root');  
     define('DB_PWD', '11111111');
-    define('DB_CHARSET', 'UTF8');  
-    define('DB_DBNAME', 'zzxd');
+    define('DB_CHARSET', 'UTF8');//设置字符编码
+    define('DB_DBNAME', 'zzxd');//选择数据库
 	
 	$con=mysql_connect(DB_HOST,DB_USER,DB_PWD);
 	if(!$con){
