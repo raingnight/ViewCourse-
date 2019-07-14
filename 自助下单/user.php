a0cc7c01e197bb91a72a6cd10f0b996dff6055b5
KIKI <46512207+raingnight@users.noreply.github.com>
2019-07-14 11:42:01 +0800

    用户查询订单页面

--- 自助下单/user.php
+++ 自助下单/user.php
@@ -4,7 +4,7 @@ header('Content-type="text/html";charset="UTF8"');
 <html>
 <body>
 <center>
-<h3>请输入账号进行订单查询</h3>
+<h3>请输入账号进行订单查询</h3> 
 <form action="user_view.php" method="post">
 	<span>账号：</span><input type="text" name="userid" placeholder="请输入账号"><br><br>
 	<input type="submit" value="确定">
