ec9a4860bcfc0b64a903de142be20b5463b2fe93
KIKI <46512207+raingnight@users.noreply.github.com>
2019-07-14 11:42:47 +0800

    输出用户查询的订单

--- 自助下单/user_view.php
+++ 自助下单/user_view.php
@@ -3,7 +3,7 @@ header('Content-type="text/html";charset="UTF8"');
 $USER_ID=$_POST['userid'];
 require_once('connect.php');
 $sql="select * from orderdetail where id = '$USER_ID'";
-$result1=mysql_query($sql);
+$result1=mysql_query($sql); 
 echo "<table><tr><td>===账号===|</td><td>===密码===|</td><td>===学校===|</td><td>===平台===|</td><td>===课程===</td></tr>";
 while($row=mysql_fetch_array($result1)){
     echo "<tr>";
