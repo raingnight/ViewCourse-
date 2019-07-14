f04ffbc0ccb2d7602ba0c7449a9e7ea15f4f707d
KIKI <46512207+raingnight@users.noreply.github.com>
2019-07-14 11:28:22 +0800

    管理员登录页面

--- 自助下单/admin.php
+++ 自助下单/admin.php
@@ -1,9 +1,11 @@
 <?php
 header('Content-type:text/html;charset="UTF8"');
 ?>
+
 <html>
 <body>
 <center>
+	<!--管理员输入账号密码查看订单数据-->
 <form action="admin_see.php" method="post">
 	<span>账号：</span><input type="text" name="userid" placeholder="请输入账号"><br><br>
 	<span>密码：</span><input type="password" name="pwd" placeholder="请输入密码"><br><br>
