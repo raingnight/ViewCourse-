3dd430d89780de35689099253d9ab01c5b0affb6
KIKI <46512207+raingnight@users.noreply.github.com>
2019-07-14 11:30:59 +0800

    管理员登录成功查看所有订单

--- 自助下单/admin_see.php
+++ 自助下单/admin_see.php
@@ -1,12 +1,16 @@
 <?php
+//获取管理员输入的账号密码
 $HOST_ID=$_POST['userid'];
 $HOST_PWD=$_POST['pwd'];
 $con=mysql_connect("localhost",$HOST_ID,$HOST_PWD);
+//判断数据库连接是否成功
 if(!$con){
 	echo "<script>alert('信息有误，请重新输入！');location='admin.php';</script>";
 }
 mysql_select_db("zzxd", $con);
+//查询所有订单
 $result1=mysql_query("SELECT * from orderdetail");
+//输出查询结果
 echo "<table><tr><td>===账号===|</td><td>===密码===|</td><td>===学校===|</td><td>===平台===|</td><td>===课程===</td></tr>";
 while($row=mysql_fetch_array($result1)){
     echo "<tr>";
